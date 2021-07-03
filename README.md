fe5a0fd6e6148b97e9629c86e7c92fbd979c63e0
lwt101101 <44947508+lwt101101@users.noreply.github.com>
2021-07-03 20:18:16 +0100

    Upload files

---
+++ README.md
@@ -0,0 +1,1 @@
+Personal website for Wentao Li
---
+++ docs/index.js
@@ -0,0 +1,41 @@
+//ANCHOR navigation
+
+window.addEventListener("resize", updateMenu);
+
+function updateMenu() {
+  const menuButton = document.querySelector(".hamburger");
+  if (!menuButton) return;
+
+  menuButton.addEventListener("click", toggleMenu);
+}
+
+function toggleMenu() {
+  const menuButton = document.querySelector(".hamburger");
+  const menu = document.querySelector(".menu__list");
+
+  if (menu.classList.contains("menu__list--displayed")) {
+    menu.classList.remove("menu__list--displayed");
+    menuButton.setAttribute("aria-expanded", "false");
+  } else {
+    menu.classList.add("menu__list--displayed");
+    menuButton.setAttribute("aria-expanded", "true");
+  }
+}
+
+updateMenu();
+
+//ANCHOR back to top button
+window.addEventListener("resize", updateToTop);
+
+function updateToTop() {
+  const backButton = document.querySelector(".top-button");
+  if (!backButton) return;
+
+  backButton.addEventListener("click", backToTop);
+}
+
+function backToTop() {
+  window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+}
+
+updateToTop();
